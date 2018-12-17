306042b59bf879178ef62c2b8f0468267b0121ba
Gabor <gabor.dobrozemszky@hotmail.com>
2018-12-17 09:02:48 +0100

    feat(compareTriplets): compares numbers of two arrays

---
+++ warmup/compare-the-triplets/compare-the-triplets.js
@@ -0,0 +1,9 @@
+function compareTriplets(a, b) {
+  let result = [0, 0];
+  for (let i = 0; i < a.length; i++) {
+      if (a[i] !== b[i]) {
+          a[i] > b[i] ? result[0]++ : result[1]++
+      }
+  }
+  return result;
+}
